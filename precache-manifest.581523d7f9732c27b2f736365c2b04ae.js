de813be8f81a15958aa7b463dbb6eea9f1066b75
DominikMistera <DominikMistera@DESKTOP-CL3ESFE>
2020-06-06 21:59:29 +0200

    Updates

---
+++ precache-manifest.581523d7f9732c27b2f736365c2b04ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37b4cf42235c890490f571fd20984cbb",
+    "url": "/labirynth/index.html"
+  },
+  {
+    "revision": "9f670946ac27b29fc3f6",
+    "url": "/labirynth/static/css/main.4cab22fd.chunk.css"
+  },
+  {
+    "revision": "5b813178f1cb8eba405a",
+    "url": "/labirynth/static/js/2.315cb819.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/labirynth/static/js/2.315cb819.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f670946ac27b29fc3f6",
+    "url": "/labirynth/static/js/main.3feeec51.chunk.js"
+  },
+  {
+    "revision": "76c15a72f3f9c18d6c2c",
+    "url": "/labirynth/static/js/runtime-main.589b571e.js"
+  }
+]);
